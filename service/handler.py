62b8c054219bf249c1582e598a706af19a153fb1
Mark Wilmes <mwilmes@mitre.org>
2020-04-16 10:24:31 -0600

    Adding processing pool to handler

--- service/handler.py
+++ service/handler.py
@@ -3,9 +3,11 @@ from service.web_svc import WebService
 from service.reg_svc import RegService
 from service.ml_svc import MLService
 from service.rest_svc import RestService
+from service.retrain_svc import RetrainingService
 from database.dao import Dao
 import os
 
+import multiprocessing as mp
 
 class ServiceHandler:
 
@@ -14,7 +16,11 @@ class ServiceHandler:
 
         self.web_svc = WebService()
         self.reg_svc = RegService(dao=self.dao)
+        self.retrain_svc = RetrainingService(dao=self.dao)
         self.data_svc = DataService(dao=self.dao, web_svc=self.web_svc)
         self.ml_svc = MLService(web_svc=self.web_svc, dao=self.dao)
         self.rest_svc = RestService(self.web_svc, self.reg_svc, self.data_svc, self.ml_svc, self.dao)
+
+        pool = mp.Process(target=self.retrain_svc.handler,daemon=True)
+        pool.start()
 
