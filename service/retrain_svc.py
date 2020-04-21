a28bb9f3fe713144fc8102ab0461ccf80a5873d2
Mark Wilmes <mwilmes@mitre.org>
2020-04-21 08:54:59 -0600

    Removing comments and unnecessary files

--- service/retrain_svc.py
+++ service/retrain_svc.py
@@ -104,43 +104,6 @@ class RetrainingService:
         model.train(X,y)
         return model
 
-    '''
-    def train_on_data(self,training_dict): # new training on data involves technique specific
-        
-        #description: method to train the boosted logistic regression models
-        #input: training data
-        #output: dictionary of models (boosted classifier)
-        
-        cv = CountVectorizer(max_features=2500)
-        tft = TfidfTransformer()
-
-        logging.info("retrain_svc: initiate training")
-        # build graph for y labels
-
-
-        for j in training_dict:
-            X_data = []
-            y_data = []
-            i = training_dict[j]
-
-            len_pos = len([i for i in y_data if i == True])
-            len_neg = len(y_data) - len_pos
-            
-            if(True in y_data and False in y_data):
-                X_train,X_test,y_train,y_test = train_test_split(np.array(X_data),np.array(y_data),test_size=0.1)
-                if(True in y_train and False in y_train):
-                    clf = ClassifierChain(GaussianNB()) #LogisticRegression(max_iter=2500, solver='lbfgs')
-                    word_counts = cv.fit_transform(X_train)
-                    tfidf = tft.fit_transform(word_counts)
-                    logging.info("retrain_svc: Fitting uid [{}] to model".format(j))
-                    clf.fit(tfidf,y_train)
-                    test_counts = cv.transform(X_test)
-                    test_tfidf = tft.transform(test_counts)
-                    #clf.score(test_counts,y_test)
-                    print("retrain_svc: Accuracy on test set = {}".format(clf.score(test_tfidf,y_test)))
-                    models[j] = (word_counts,clf)
-        return models
-        '''
 
     def train(self): 
         '''
@@ -150,14 +113,14 @@ class RetrainingService:
         '''
         while(True):
             time_check = time.localtime(time.time())
-            #if(time_check[3] == 12 and time_check[4] == 0): # kick off training at noon and midnight
-            raw_data = self.get_training_data()
-            #print(raw_data)
-            models = self.train_model(raw_data) #self.train_on_data(raw_data)
-            self.save_current_model(models)
-            logging.info("retrain_svc: Retraining task finished")
-            #else:
-            #    time.sleep(10)
+            if(time_check[3] == 12 and time_check[4] == 0): # kick off training at noon and midnight
+                raw_data = self.get_training_data()
+                #print(raw_data)
+                models = self.train_model(raw_data) #self.train_on_data(raw_data)
+                self.save_current_model(models)
+                logging.info("retrain_svc: Retraining task finished")
+            else:
+                time.sleep(10)
 
     def handler(self):
         self.train()
